5106211bfad83e1e60b70d047034fb661c70193e
Vaishnavi Nema <vaishnavinema27@gmail.com>
2026-03-21 13:50:58 +0530

    phase 2 - mongodb connection setup

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const connectDB=require('./config/db');
 
 dotenv.config();  //dotenv.config()`** → loads all variables from your `.env` file into `process.env`
 
 
 const PORT = process.env.PORT || 8000;     //process.env.PORT → reads the PORT value from your .env file. || 8000 means if PORT is not set, use 8000 as default
 
-
+connectDB();
 app.listen(PORT, () => {    //actually starts the server on that port and keeps it running
   console.log(`Server is running on port ${PORT}`);
   console.log(`Environment: ${process.env.NODE_ENV}`);
